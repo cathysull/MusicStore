8ff42428649e3dbac71fd1822378b12bf182eee3
cathysull <cathys@live.com>
2015-04-27 17:00:09 -0700

    Initial commit

---
+++ Gracenote Integration Service/GracenoteRequests.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "GracenoteRequests.h"
+
+
+GracenoteRequests::GracenoteRequests()
+{
+}
+
+
+GracenoteRequests::~GracenoteRequests()
+{
+}
---
+++ Gracenote Integration Service/GracenoteRequests.h
@@ -0,0 +1,8 @@
+#pragma once
+class GracenoteRequests
+{
+public:
+	GracenoteRequests();
+	~GracenoteRequests();
+};
+
